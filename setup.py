39a7696a27b6ce1bf5ba2ed72fbfe5d862072619
Mahika-07 <mahikapatney@gmail.com>
2025-04-10 08:27:54 +0530

    Initial code

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'QApplication',
+    version= '0.0.1',
+    author= 'Mahika Patney',
+    author_email= 'mahikapatney@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
